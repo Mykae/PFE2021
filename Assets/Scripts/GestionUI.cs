bfe8e55c87e223828705cfde46bccf44f9ada344
Àre <arematurana@hotmail.com>
2022-01-24 22:39:43 +0100

    UI_Quetes

---
+++ Assets/Scripts/GestionUI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestionUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tableauDeQuetes;
+    private GameObject queteEnCours;
+
+    //Quêtes Timmy
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        queteEnCours = tableauDeQuetes.transform.GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            AvancerQuete(1);
+    }
+
+    public void AvancerQuete(int indiceQuete)
+    {
+        queteEnCours = tableauDeQuetes.transform.GetChild(indiceQuete).gameObject;
+
+        //Si la quête est finie on quitte la méthode
+        if (queteEnCours.activeSelf == false)
+            return;
+
+        //Quetes -> Avancement (child 1) -> ValeurActuelle (child 0)
+        var valeurActuelle = queteEnCours.transform.GetChild(1).transform.GetChild(0).GetComponent<Text>();
+
+        //Quetes -> Avancement (child 1) -> Total (child 1)
+        var total = queteEnCours.transform.GetChild(1).transform.GetChild(1).GetComponent<Text>();
+
+        //On converti le texte de valeurActuelle en int pour l'incrémenter puis on le remet à sa place
+        int increment = System.Convert.ToInt32(valeurActuelle.text) + 1;
+        valeurActuelle.text = "" + increment;
+
+        //Si on a atteint l'objectif on désactive l'avancement et active l'interface de quête réussie
+        if (valeurActuelle.text == total.text)
+        {
+            queteEnCours.transform.GetChild(1).gameObject.SetActive(false);
+            queteEnCours.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
