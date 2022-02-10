5fbf6474ace626b662abfcea0fd93390c90e964e
Mikael <mikael.landa22@gmail.com>
2022-02-10 08:52:57 +0100

    Update FIX height change

--- Assets/Mika/Script/HeightChanger.cs
+++ Assets/Mika/Script/HeightChanger.cs
@@ -10,12 +10,30 @@ public class HeightChanger : MonoBehaviour
     public float yPosEnter;
     public float yPosExit;
 
+    private Vector3 getRelativePosition(Transform origin, Vector3 position)
+    {
+        Vector3 distance = position - origin.position;
+        Vector3 relativePosition = Vector3.zero;
+        relativePosition.x = Vector3.Dot(distance, origin.right.normalized);
+        relativePosition.y = Vector3.Dot(distance, origin.up.normalized);
+        relativePosition.z = Vector3.Dot(distance, origin.forward.normalized);
+
+        return relativePosition;
+    }
+
+
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        yPosEnter = col.bounds.center.y;
+        yPosEnter = Mathf.Sign(getRelativePosition(this.gameObject.transform, col.gameObject.transform.position).y);
         if(col.gameObject.tag == "Player")
         {
-            if (col.GetComponent<PlayerMovement>().height == fromLayer && isPassed == false)
+            if(isPassed == false)
+            {
+                isPassed = true;
+            }
+
+            /*if (col.GetComponent<PlayerMovement>().height == fromLayer && isPassed == false)
             {
                 col.GetComponent<PlayerMovement>().SetHeight(toLayer);
                 isPassed = true;
@@ -24,18 +42,27 @@ public class HeightChanger : MonoBehaviour
             {
                 col.GetComponent<PlayerMovement>().SetHeight(fromLayer);
                 isPassed = true;
-            }
+            }*/
         }
     }
 
     private void OnTriggerExit2D(Collider2D col)
     {
-        yPosExit = col.bounds.center.y;
-        if (Mathf.Sign(yPosEnter) != Mathf.Sign(yPosExit))
+        if (col.gameObject.tag == "Player")
         {
+            yPosExit = Mathf.Sign(getRelativePosition(this.gameObject.transform, col.gameObject.transform.position).y);
+            if (yPosEnter != yPosExit)
+            {
+                if (col.GetComponent<PlayerMovement>().height == fromLayer)
+                {
+                    col.GetComponent<PlayerMovement>().SetHeight(toLayer);
+                }
+                else if (col.GetComponent<PlayerMovement>().height == toLayer)
+                {
+                    col.GetComponent<PlayerMovement>().SetHeight(fromLayer);
+                }
+            }
             isPassed = false;
-            yPosEnter = 0f;
-            yPosExit = 0f;
         }
     }
 }
