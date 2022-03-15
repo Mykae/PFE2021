5d4494c756af3613375061bf67e498f7b8123dba
Mikael <mikael.landa22@gmail.com>
2022-03-15 11:07:50 +0100

    WIP Fruits

---
+++ Assets/Mika/Script/Old Scripts/Spawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject[] hazards;
+
+    private float timeBetweenSpawns;
+    public float startTimeBetweenSpawns;
+
+    public float minTimeBetweenSpawns;
+    public float decrease;
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            if (timeBetweenSpawns <= 0)
+            {
+                Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+                GameObject randomHazard = hazards[Random.Range(0, hazards.Length)];
+
+                Instantiate(randomHazard, randomSpawnPoint.position, Quaternion.identity);
+
+                if (startTimeBetweenSpawns > minTimeBetweenSpawns)
+                {
+                    startTimeBetweenSpawns -= decrease;
+                }
+
+                timeBetweenSpawns = startTimeBetweenSpawns;
+            }
+            else
+            {
+                timeBetweenSpawns -= Time.deltaTime;
+            }
+        }
+    }
+}
